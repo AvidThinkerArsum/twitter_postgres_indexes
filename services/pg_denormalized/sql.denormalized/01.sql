25d9e2403a34e193473864cd183c621a674ee1da
Arsum Nadeem Chaudhary <arsumnadeemch16203@gmail.com>
2024-05-12 10:45:10 -0700

    done

--- services/pg_denormalized/sql.denormalized/01.sql
+++
@@ -1,3 +0,0 @@
-SELECT count(DISTINCT data->>'id')
-FROM tweets_jsonb
-WHERE data->'entities'->'hashtags' @@ '$[*].text == "coronavirus"' OR data ->'extended_tweet'->'entities'->'hashtags' @@ '$[*].text == "coronavirus"';
